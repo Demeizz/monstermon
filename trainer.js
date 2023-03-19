567e13fd766ea0f5af2ad2c5d48e3f1336fd19df
Demeiz <126128293+Demeizz@users.noreply.github.com>
2023-03-19 01:20:23 -0500

    Inicio del codigo Js
    Agregado la funcionalidad de los botones

--- trainer.js
+++ trainer.js
@@ -1,2 +1,12 @@
-alert("Inserte codigo js")
-console.log("Editando y probando")
+function confirmarMonstermon() {
+    alert("Seleccionaste tu monstermon")
+}
+function gameOver() {
+    respuesta=prompt("Seguro quieres empezar de nuevo?")
+}
+
+let buttonConfirm = document.getElementById("button-confirm")
+buttonConfirm.addEventListener("click",confirmarMonstermon)
+
+let buttonRestard = document.getElementById("button-yes")
+buttonRestard.addEventListener("click",gameOver)
